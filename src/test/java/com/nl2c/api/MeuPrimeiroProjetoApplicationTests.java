ff43a22041a7bd96e92bd30d648fffec0a9b9ca5
César Correia <orthoprocnlc@gmail.com>
2021-07-29 00:03:11 +0100

    Arquivos iniciais do Projeto

---
+++ src/test/java/com/nl2c/api/MeuPrimeiroProjetoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nl2c.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MeuPrimeiroProjetoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
